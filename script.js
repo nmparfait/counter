98e4f6c7dad6ba258a1a6095b05c37e1ebb5fe64
Parfait NM <mbamu.developer@gmail.com>
2022-02-20 23:20:26 -0500

    better adding functions in methods than in html

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoalA: 'Finish the course and learn Vue!',
-            courseGoalB: 'Master Vue and create great Apps',
-            vueLink: 'https://vuejs.org',
-            autor: 'https://github.com/nmparfait',
+            
             counter: 0,
         };
     },
 
     methods: {
-        outputGoal(){
-            const randomNumber = Math.random();
-            if (randomNumber < 0.5) {
-                return this.courseGoalA;
-            } else {
-                return this.courseGoalB;
-            }
+        add(){
+            this.counter ++
+        },
+        reduce(){
+            this.counter --
         }
 
     }
