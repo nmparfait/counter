5b173be4cc2353c487c659520a4e22a7c05db420
Parfait NM <mbamu.developer@gmail.com>
2022-02-21 11:23:00 -0500

    input the name

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            
             counter: 0,
+            name: ''
         };
     },
 
     methods: {
+        setName(event, lastName){
+            this.name = event.target.value + '' + lastName;
+        }
+        ,
         add(num){
             this.counter = this.counter + num;
         },
